f25a4a3d8d4500da859a08c461d8d13da0f2d16d
b0n541 <b0n541@github>
2013-06-11 22:11:25 +0200

    first test with elastic search

---
+++ src/main/java/StartTest.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.elasticsearch.ElasticSearchException;
+import org.elasticsearch.action.search.SearchResponse;
+
+import server.db.GameRepository;
+
+public class StartTest {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws ElasticSearchException
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws ElasticSearchException,
+			IOException, InterruptedException {
+
+		GameRepository gameRepository = new GameRepository();
+		gameRepository.init();
+
+		System.out.println(gameRepository.getGameCount() + " games in db.");
+
+		System.out
+				.println("You can search after player names or for game details. Type 'detail' followed by search string.");
+
+		handleConsoleInputs(gameRepository);
+
+		gameRepository.shutDown();
+	}
+
+	private static void handleConsoleInputs(GameRepository gameRepository) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = null;
+		try {
+			while ((line = br.readLine()) != null && line.length() > 0) {
+				if (line.startsWith("detail")) {
+					searchForDetail(gameRepository, line.replace("detail ", ""));
+				} else {
+					searchForUser(gameRepository, line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void searchForDetail(GameRepository gameRepository,
+			String line) {
+		SearchResponse searchForUser = gameRepository.searchForDetail(line);
+		System.out.println(searchForUser.getHits().getTotalHits()
+				+ " games found for string like " + line + ".");
+		printStatisticsAndFirstHit(searchForUser);
+	}
+
+	private static void printStatisticsAndFirstHit(SearchResponse searchForUser) {
+		System.out.println(searchForUser.getTook().getMillis() + " ms");
+		if (searchForUser.getHits().getTotalHits() > 0) {
+			System.out.println("First hit: "
+					+ searchForUser.getHits().iterator().next()
+							.sourceAsString());
+		}
+	}
+
+	private static void searchForUser(GameRepository gameRepository, String line) {
+		SearchResponse searchForUser = gameRepository.searchForUser(line);
+		System.out.println(searchForUser.getHits().getTotalHits()
+				+ " games found for " + line + " playing.");
+		printStatisticsAndFirstHit(searchForUser);
+	}
+}
---
+++ src/main/java/server/db/GameRepository.java
@@ -0,0 +1,221 @@
+package server.db;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequestBuilder;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+import org.elasticsearch.action.admin.indices.refresh.RefreshResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.FieldQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.node.Node;
+
+public class GameRepository {
+
+	private final static String ISS = "iss";
+	private final static String GAMES = "games";
+
+	private final Node node;
+	private final Client client;
+
+	public GameRepository() {
+		// on startup
+		node = nodeBuilder().node();
+		client = node.client();
+
+		waitUntilIndexesAreInitialized();
+	}
+
+	private void waitUntilIndexesAreInitialized() {
+		ClusterHealthRequestBuilder prepareHealth = client.admin().cluster()
+				.prepareHealth(ISS);
+		ClusterHealthResponse healtResponse = prepareHealth
+				.setWaitForGreenStatus().execute().actionGet();
+	}
+
+	/**
+	 * Imports the ISS data set if not available.
+	 */
+	public void init() {
+		if (!issDataAvailable(ISS)) {
+			System.out.println("No ISS data available, importing them now...");
+			importData();
+		}
+	}
+
+	private boolean issDataAvailable(String index) {
+		IndicesExistsRequest indicesExistsRequest = new IndicesExistsRequest(
+				index);
+		ActionFuture<IndicesExistsResponse> exists = client.admin().indices()
+				.exists(indicesExistsRequest);
+		IndicesExistsResponse indicesExistsResponse = exists.actionGet();
+		return indicesExistsResponse.isExists();
+	}
+
+	private void importData() {
+
+		BufferedReader br = getFile();
+
+		int lineCount = 0;
+		String strLine;
+
+		BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+		try {
+			while ((strLine = br.readLine()) != null) {
+
+				XContentBuilder builder = createJson(strLine);
+
+				bulkRequest.add(client.prepareIndex(ISS, GAMES).setSource(
+						builder));
+
+				lineCount++;
+
+				if (lineCount % 10000 == 0) {
+					BulkResponse response = bulkRequest.execute().actionGet();
+					if (!response.hasFailures()) {
+						System.out.println(lineCount + " games imported.");
+						bulkRequest = client.prepareBulk();
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (bulkRequest.numberOfActions() > 0) {
+			BulkResponse response = bulkRequest.execute().actionGet();
+			if (!response.hasFailures()) {
+				System.out.println(lineCount + " games imported.");
+				bulkRequest = client.prepareBulk();
+			}
+		}
+
+		refreshIndex(ISS);
+	}
+
+	private void refreshIndex(String index) {
+		RefreshRequest request = new RefreshRequest(index);
+		ActionFuture<RefreshResponse> refresh = client.admin().indices()
+				.refresh(request);
+		refresh.actionGet();
+	}
+
+	private XContentBuilder createJson(String strLine) throws IOException {
+		XContentBuilder builder = jsonBuilder().startObject();
+
+		parseLine(builder, strLine);
+
+		builder.field("completegame", strLine);
+		builder.endObject();
+		return builder;
+	}
+
+	private BufferedReader getFile() {
+		InputStream inputStream = this.getClass().getClassLoader()
+				.getResourceAsStream("issgames.sgf");
+		DataInputStream in = new DataInputStream(inputStream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		return br;
+	}
+
+	private void parseLine(XContentBuilder builder, String game)
+			throws IOException {
+
+		final Pattern summaryPartPattern = Pattern.compile("(\\w+)\\[(.*?)\\]"); //$NON-NLS-1$
+		final Matcher summaryPartMatcher = summaryPartPattern.matcher(game);
+
+		while (summaryPartMatcher.find()) {
+
+			final String summaryPartMarker = summaryPartMatcher.group(1);
+			final String summeryPart = summaryPartMatcher.group(2);
+
+			parseSummaryPart(builder, summaryPartMarker, summeryPart);
+		}
+	}
+
+	private static void parseSummaryPart(XContentBuilder builder,
+			String summaryPartMarker, String summaryPart) throws IOException {
+
+		if ("ID".equals(summaryPartMarker)) { //$NON-NLS-1$
+			builder.field("gameID", summaryPart);
+		} else if ("P0".equals(summaryPartMarker)) {
+			builder.field("forehand", summaryPart);
+		} else if ("P1".equals(summaryPartMarker)) {
+			builder.field("middlehand", summaryPart);
+		} else if ("P2".equals(summaryPartMarker)) {
+			builder.field("rearhand", summaryPart);
+		}
+	}
+
+	/**
+	 * Gets the total count of games in database.
+	 * 
+	 * @return Count of games
+	 */
+	public long getGameCount() {
+		SearchResponse response = client.prepareSearch().execute().actionGet();
+		return response.getHits().getTotalHits();
+	}
+
+	/**
+	 * Shuts database down.
+	 */
+	public void shutDown() {
+		// on shutdown
+		node.close();
+	}
+
+	/**
+	 * Searches for games a user made on ISS.
+	 * 
+	 * @param user
+	 *            Login name of user
+	 * @return Games the user took part
+	 */
+	public SearchResponse searchForUser(String user) {
+
+		QueryBuilder qb = QueryBuilders.multiMatchQuery(user, "forehand",
+				"middlehand", "rearhand");
+
+		return executeQuery(qb);
+	}
+
+	/**
+	 * Searches for a detail in the game summary (ISS format).
+	 * 
+	 * @param detail
+	 *            Search string
+	 * @return Games where the detail is found
+	 */
+	public SearchResponse searchForDetail(String detail) {
+		FieldQueryBuilder qb = QueryBuilders.fieldQuery("completegame", detail);
+
+		return executeQuery(qb);
+	}
+
+	private SearchResponse executeQuery(QueryBuilder qb) {
+		SearchResponse searchResponse = client.prepareSearch("iss")
+				.setTypes("games").setQuery(qb).execute().actionGet();
+
+		return searchResponse;
+	}
+}
